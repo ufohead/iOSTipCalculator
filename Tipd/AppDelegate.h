1613bfb1f17721a95c91aae3caa139f131ea0427
Ufohead Tseng <ufohead@yahoo-inc.com>
2015-05-14 12:22:55 +0800

    Initial Commit

---
+++ Tipd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tipd
+//
+//  Created by Ufohead Tseng on 2015/5/14.
+//  Copyright (c) 2015年 Ufohead Tseng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tipd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipd
+//
+//  Created by Ufohead Tseng on 2015/5/14.
+//  Copyright (c) 2015年 Ufohead Tseng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
